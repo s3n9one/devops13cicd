c44e1517ed73ec2700e063a2de72e8ff965e6304
s3n9one <paramosha228@gmail.com>
2024-12-20 11:42:17 +0500

    Добавил комментарии

--- server/application.py
+++ server/application.py
@@ -1,12 +1,16 @@
+"""Module for demonstration purposes"""
 import http.server
 import socketserver
 
 
 PORT = 8000
 
+"""Test class for demonstrarion purposes"""
 class TestMe():
+    """Return the number 5"""
     def take_five(self):
         return 5
+    """Return the port number"""
     def port(self):
         return PORT
 
